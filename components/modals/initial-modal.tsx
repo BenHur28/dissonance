d42a87200dbf6a358c4e19d53272bc743d8e8661
Ben Hur <bhur777@gmail.com>
2023-10-06 01:20:17 -0400

    Add hydration error prevention

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -24,6 +24,7 @@ import {
 
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { useEffect, useState } from "react";
 
 const formSchema = z.object({
 	name: z.string().min(1, {
@@ -35,6 +36,12 @@ const formSchema = z.object({
 });
 
 const InitialModal = () => {
+	const [isMounted, setIsMounted] = useState(false);
+
+	useEffect(() => {
+		setIsMounted(true);
+	}, []);
+
 	const form = useForm({
 		resolver: zodResolver(formSchema),
 		defaultValues: {
@@ -48,6 +55,10 @@ const InitialModal = () => {
 	const onSubmit = async (values: z.infer<typeof formSchema>) => {
 		console.log(values);
 	};
+
+	if (!isMounted) {
+		return null;
+	}
 
 	return (
 		<Dialog open={true}>
