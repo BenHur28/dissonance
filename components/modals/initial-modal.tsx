e4377efef4d2c3b8a1c334b460032176202d29dc
Ben Hur <bhur777@gmail.com>
2023-10-06 01:03:06 -0400

    Create dialog

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -1,7 +1,34 @@
 "use client";
 
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+import {
+	Dialog,
+	DialogContent,
+	DialogDescription,
+	DialogFooter,
+	DialogHeader,
+	DialogTitle,
+} from "@/components/ui/dialog";
+
 const InitialModal = () => {
-	return <div>This will be a modal!</div>;
+	return (
+		<Dialog open={true}>
+			<DialogContent className="bg-white text-black p-0 overflow-hidden">
+				<DialogHeader className="pt-8 px-6">
+					<DialogTitle className="text-center text-2xl font-bold">
+						Customize your server
+					</DialogTitle>
+					<DialogDescription className="text-center text-zinc-500">
+						Give your server a personality with a name and an image. You can
+						always change it later.
+					</DialogDescription>
+				</DialogHeader>
+			</DialogContent>
+		</Dialog>
+	);
 };
 
 export default InitialModal;
