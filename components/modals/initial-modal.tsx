9b2fe9b64299d6051bdaafa8baa013eb1981b76a
Ben Hur <bhur777@gmail.com>
2023-10-06 01:17:56 -0400

    Create Form on initial-modal.tsx

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -13,6 +13,18 @@ import {
 	DialogTitle,
 } from "@/components/ui/dialog";
 
+import {
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from "@/components/ui/form";
+
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
 const formSchema = z.object({
 	name: z.string().min(1, {
 		message: "Server name is required.",
@@ -30,6 +42,13 @@ const InitialModal = () => {
 			imageUrl: "",
 		},
 	});
+
+	const isLoading = form.formState.isSubmitting;
+
+	const onSubmit = async (values: z.infer<typeof formSchema>) => {
+		console.log(values);
+	};
+
 	return (
 		<Dialog open={true}>
 			<DialogContent className="bg-white text-black p-0 overflow-hidden">
@@ -42,6 +61,40 @@ const InitialModal = () => {
 						always change it later.
 					</DialogDescription>
 				</DialogHeader>
+				<Form {...form}>
+					<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+						<div className="space-y-8 px-6">
+							<div className="flex items-center justify-center text-center">
+								IMAGE UPLOAD COMPONENT GOES HERE
+							</div>
+							<FormField
+								control={form.control}
+								name="name"
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70">
+											Server name
+										</FormLabel>
+										<FormControl>
+											<Input
+												disabled={isLoading}
+												className="text-black bg-zinc-300/50 border-0 focus-visible:ring-0 focus-visible:ring-offset-0"
+												placeholder="Enter server name"
+												{...field}
+											/>
+										</FormControl>
+										<FormMessage />
+									</FormItem>
+								)}
+							/>
+						</div>
+						<DialogFooter className="bg-gray-100 px-6 py-4">
+							<Button disabled={isLoading} variant="primary">
+								Create
+							</Button>
+						</DialogFooter>
+					</form>
+				</Form>
 			</DialogContent>
 		</Dialog>
 	);
