1f1c559120f7c5fb74f27b81820739055bea9116
Ben Hur <bhur777@gmail.com>
2023-10-07 02:41:04 -0400

    Finish server creation route.ts

--- app/api/servers/route.ts
+++ app/api/servers/route.ts
@@ -2,6 +2,7 @@ import { v4 as uuidv4 } from "uuid";
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { NextResponse } from "next/server";
+import { MemberRole } from "@prisma/client";
 
 export async function POST(req: Request) {
 	try {
@@ -11,6 +12,23 @@ export async function POST(req: Request) {
 		if (!profile) {
 			return new NextResponse("Unauthorized", { status: 401 });
 		}
+
+		const server = await db.server.create({
+			data: {
+				profileId: profile.id,
+				name: name,
+				imageUrl: imageUrl,
+				inviteCode: uuidv4(),
+				channels: {
+					create: [{ name: "General", profileId: profile.id }],
+				},
+				members: {
+					create: [{ profileId: profile.id, role: MemberRole.ADMIN }],
+				},
+			},
+		});
+
+		return NextResponse.json(server);
 	} catch (error) {
 		console.log("[SERVERS_POST]", error);
 		return new NextResponse("Internal Error", { status: 500 });
