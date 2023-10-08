da9ef9a7055a024b8a01aeda61d203f234707589
Ben Hur <bhur777@gmail.com>
2023-10-08 04:35:15 -0400

    show tooltip for server name

--- components/navigation/nav-item.tsx
+++ components/navigation/nav-item.tsx
@@ -12,7 +12,24 @@ type NavItemProps = {
 };
 
 const Navitem = ({ id, imageUrl, name }: NavItemProps) => {
-	return <div>Navitem</div>;
+	const params = useParams();
+	const router = useRouter();
+
+	return (
+		<ActionTooltip side="right" align="center" label={name}>
+			<button onClick={() => {}} className="group relative flex items-center">
+				<div
+					className={cn(
+						"absolute left-0 bg-priamry rounded-r-full transition-all w-[4px]",
+						params?.serverId != id && "group-hover:h-[20px]",
+						params?.serverId === id ? "h-[36px]" : "h-[8px]"
+					)}
+				>
+					server
+				</div>
+			</button>
+		</ActionTooltip>
+	);
 };
 
 export default Navitem;
