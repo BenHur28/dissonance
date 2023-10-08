d963770e4ecf00ac429257e7ddae1e1287c4f611
Ben Hur <bhur777@gmail.com>
2023-10-08 04:03:56 -0400

    Create nav-sidebar component

--- app/(main)/(routes)/layout.tsx
+++ app/(main)/(routes)/layout.tsx
@@ -1,7 +1,11 @@
+import Navsidebar from "@/components/navigation/nav-sidebar";
+
 const MainLayout = async ({ children }: { children: React.ReactNode }) => {
 	return (
 		<div className="h-full">
-			<div className="hidden md:flex h-full w-[72px] z-30 flex-col fixed inset-y-0"></div>
+			<div className="hidden md:flex h-full w-[72px] z-30 flex-col fixed inset-y-0">
+				<Navsidebar />
+			</div>
 			<main className="md:pl-[72px] h-full">{children}</main>
 		</div>
 	);
---
+++ components/navigation/nav-sidebar.tsx
@@ -0,0 +1,5 @@
+const Navsidebar = () => {
+	return <div>Nav Side Bar</div>;
+};
+
+export default Navsidebar;
