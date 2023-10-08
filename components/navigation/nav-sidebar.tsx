f1e0294eecbd448469c55a1d78734f702611c1bd
Ben Hur <bhur777@gmail.com>
2023-10-08 04:25:04 -0400

    add separator

--- components/navigation/nav-sidebar.tsx
+++ components/navigation/nav-sidebar.tsx
@@ -2,6 +2,7 @@ import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { redirect } from "next/navigation";
 import NavAction from "./nav-action";
+import { Separator } from "../ui/separator";
 
 const Navsidebar = async () => {
 	const profile = await currentProfile();
@@ -22,6 +23,7 @@ const Navsidebar = async () => {
 	return (
 		<div className="space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1e1f22] py-3">
 			<NavAction />
+			<Separator className="h-[2px] bg-zine-300 dark:bg-zinc-700 rounded-md w-10 mx-auto" />
 		</div>
 	);
 };
