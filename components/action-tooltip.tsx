eb4463b75a7a60ac171a13b6bc49f12d535397a2
Ben Hur <bhur777@gmail.com>
2023-10-08 04:21:47 -0400

    add tooltip to sidenav

--- components/action-tooltip.tsx
+++ components/action-tooltip.tsx
@@ -20,14 +20,16 @@ export const ActionTooltip = ({
 	side,
 	align,
 }: ActionToolTipProps) => {
-	<TooltipProvider>
-		<Tooltip delayDuration={50}>
-			<TooltipTrigger asChild>{children}</TooltipTrigger>
-			<TooltipContent side={side} align={align}>
-				<p className="font-semibold text-sm capitalize">
-					{label.toLowerCase()}
-				</p>
-			</TooltipContent>
-		</Tooltip>
-	</TooltipProvider>;
+	return (
+		<TooltipProvider>
+			<Tooltip delayDuration={50}>
+				<TooltipTrigger asChild>{children}</TooltipTrigger>
+				<TooltipContent side={side} align={align}>
+					<p className="font-semibold text-sm capitalize">
+						{label.toLowerCase()}
+					</p>
+				</TooltipContent>
+			</Tooltip>
+		</TooltipProvider>
+	);
 };
