ffb7e4d5e39f3d2d1d020901e6517e8e12705ae1
Ben Hur <bhur777@gmail.com>
2023-10-10 02:35:57 -0400

    Add serverside bar to layout page

--- app/(main)/(routes)/servers/[serverId]/layout.tsx
+++ app/(main)/(routes)/servers/[serverId]/layout.tsx
@@ -1,3 +1,4 @@
+import ServerSideBar from "@/components/server/server-sidebar";
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { redirectToSignIn } from "@clerk/nextjs";
@@ -34,9 +35,9 @@ const ServerIdLayout = async ({
 	return (
 		<div className="h-full">
 			<div className="hidden md:flex h-full w-60 z-20 flex-col fixed inset-y-0">
-				Server Side Bar
-				<main className="h-full md:pl-60">{children}</main>
+				<ServerSideBar />
 			</div>
+			<main className="h-full md:pl-60">{children}</main>
 		</div>
 	);
 };
