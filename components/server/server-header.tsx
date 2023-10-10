2f1299f437a31f45e2ce7d1bdf7942d15045545c
Ben Hur <bhur777@gmail.com>
2023-10-10 03:20:19 -0400

    Add leave server and delete server based on roles

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -4,8 +4,10 @@ import { ServerWithMembersWithProfiles } from "@/types";
 import { MemberRole } from "@prisma/client";
 import {
 	ChevronDown,
+	LogOut,
 	PlusCircle,
 	Settings,
+	Trash,
 	UserPlus,
 	Users,
 } from "lucide-react";
@@ -13,6 +15,7 @@ import {
 	DropdownMenu,
 	DropdownMenuContent,
 	DropdownMenuItem,
+	DropdownMenuSeparator,
 	DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 
@@ -58,6 +61,19 @@ const ServerHeader = ({ server, role }: ServerHeaderProps) => {
 					<DropdownMenuItem className="text-primary px-3 py-2 text-sm cursor-pointer">
 						Create Channel
 						<PlusCircle className="h-4 w-4 ml-auto" />
+					</DropdownMenuItem>
+				)}
+				{isModerator && <DropdownMenuSeparator />}
+				{!isAdmin && (
+					<DropdownMenuItem className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
+						Leave Server
+						<LogOut className="h-4 w-4 ml-auto" />
+					</DropdownMenuItem>
+				)}
+				{isAdmin && (
+					<DropdownMenuItem className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
+						Delete Server
+						<Trash className="h-4 w-4 ml-auto" />
 					</DropdownMenuItem>
 				)}
 			</DropdownMenuContent>
