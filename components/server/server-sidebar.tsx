75c2e61a295e70f272006b68606dfeea8639a8fc
Ben Hur <bhur777@gmail.com>
2023-10-10 02:55:33 -0400

    Add channels, members, role stylize

--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -1,5 +1,6 @@
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
+import { ChannelType } from "@prisma/client";
 import { redirect } from "next/navigation";
 
 type ServerSidebarProps = {
@@ -34,7 +35,31 @@ const ServerSideBar = async ({ serverId }: ServerSidebarProps) => {
 		},
 	});
 
-	return <div>ServerSideBar</div>;
+	if (!server) {
+		return redirect("/");
+	}
+
+	const textChannels = server?.channels.filter(
+		(channel) => channel.type === ChannelType.TEXT
+	);
+	const audioChannels = server?.channels.filter(
+		(channel) => channel.type === ChannelType.AUDIO
+	);
+	const videoChannels = server?.channels.filter(
+		(channel) => channel.type === ChannelType.VIDEO
+	);
+	const members = server?.members.filter(
+		(member) => member.profileId !== profile.id
+	);
+	const role = server.members.find(
+		(member) => member.profileId === profile.id
+	)?.role;
+
+	return (
+		<div className="flex flex-col h-full text-primary w-full dark:bg-[#2B2D31] bg-[#F2F3F5]">
+			ServerSideBar
+		</div>
+	);
 };
 
 export default ServerSideBar;
