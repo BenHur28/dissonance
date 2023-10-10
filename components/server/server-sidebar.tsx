548b689eccd90cf979c8524741e5dede10152c16
Ben Hur <bhur777@gmail.com>
2023-10-10 02:50:11 -0400

    Get server, profile

--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -1,4 +1,39 @@
-const ServerSideBar = () => {
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
+
+type ServerSidebarProps = {
+	serverId: string;
+};
+
+const ServerSideBar = async ({ serverId }: ServerSidebarProps) => {
+	const profile = await currentProfile();
+
+	if (!profile) {
+		return redirect("/");
+	}
+
+	const server = await db.server.findUnique({
+		where: {
+			id: serverId,
+		},
+		include: {
+			channels: {
+				orderBy: {
+					createdAt: "asc",
+				},
+			},
+			members: {
+				include: {
+					profile: true,
+				},
+				orderBy: {
+					role: "asc",
+				},
+			},
+		},
+	});
+
 	return <div>ServerSideBar</div>;
 };
 
