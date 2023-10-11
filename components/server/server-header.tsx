db4081e650fa3bb12a6b627f7bd1eea7f8f5eec8
Ben Hur <bhur777@gmail.com>
2023-10-10 23:55:54 -0400

    add onOpen to onClick for invite people dropdown

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -18,6 +18,7 @@ import {
 	DropdownMenuSeparator,
 	DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { useModal } from "@/hooks/use-modal-store";
 
 type ServerHeaderProps = {
 	server: ServerWithMembersWithProfiles;
@@ -25,6 +26,8 @@ type ServerHeaderProps = {
 };
 
 const ServerHeader = ({ server, role }: ServerHeaderProps) => {
+	const { onOpen } = useModal();
+
 	const isAdmin = role === MemberRole.ADMIN;
 	const isModerator = isAdmin || role === MemberRole.MODERATOR;
 	return (
@@ -40,7 +43,10 @@ const ServerHeader = ({ server, role }: ServerHeaderProps) => {
 			</DropdownMenuTrigger>
 			<DropdownMenuContent className="w-56 text-xs font-medium text-black dark:text-neurtal-400 space-y-[2px]">
 				{isModerator && (
-					<DropdownMenuItem className="text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer">
+					<DropdownMenuItem
+						onClick={() => onOpen("invite", { server })}
+						className="text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer"
+					>
 						Invite People
 						<UserPlus className="h-4 w-4 ml-auto" />
 					</DropdownMenuItem>
