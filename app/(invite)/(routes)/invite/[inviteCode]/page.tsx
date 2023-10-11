bf3e69943c5b2dbf6348dd8c15f7778c3da08ca0
Ben Hur <bhur777@gmail.com>
2023-10-11 00:35:45 -0400

    Check to see if current user is part of the server

--- app/(invite)/(routes)/invite/[inviteCode]/page.tsx
+++ app/(invite)/(routes)/invite/[inviteCode]/page.tsx
@@ -1,4 +1,39 @@
-const InviteCodePage = () => {
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+type InviteCodePageProps = {
+	params: {
+		inviteCode: string;
+	};
+};
+
+const InviteCodePage = async ({ params }: InviteCodePageProps) => {
+	const profile = await currentProfile();
+	if (!profile) {
+		return redirectToSignIn;
+	}
+
+	if (!params.inviteCode) {
+		return redirect("/");
+	}
+
+	const existingServer = await db.server.findFirst({
+		where: {
+			inviteCode: params.inviteCode,
+			members: {
+				some: {
+					profileId: profile.id,
+				},
+			},
+		},
+	});
+
+	if (existingServer) {
+		return redirect(`/servers/${existingServer.id}`);
+	}
+
 	return <div>InviteCodePage</div>;
 };
 
