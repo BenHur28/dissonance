03c0b54ad464a7ddfba567a067b4718e7cca658e
Ben Hur <bhur777@gmail.com>
2023-10-11 00:23:17 -0400

    Create onNew function for generating new link

--- components/modals/invite-modal.tsx
+++ components/modals/invite-modal.tsx
@@ -14,9 +14,11 @@ import { Button } from "../ui/button";
 import { Check, Copy, RefreshCw } from "lucide-react";
 import { useOrigin } from "@/hooks/use-origin";
 import { useState } from "react";
+import axios from "axios";
+import { Island_Moments } from "next/font/google";
 
 const InviteModal = () => {
-	const { isOpen, onClose, type, data } = useModal();
+	const { onOpen, isOpen, onClose, type, data } = useModal();
 	const origin = useOrigin();
 
 	const isModalOpen = isOpen && type === "invite";
@@ -36,6 +38,20 @@ const InviteModal = () => {
 		}, 2000);
 	};
 
+	const onNew = async () => {
+		try {
+			setIsLoading(true);
+			const response = await axios.patch(
+				`/api/servers/${server?.id}/invite-code`
+			);
+			onOpen("invite", { server: response.data });
+		} catch (error) {
+			console.log(error);
+		} finally {
+			setIsLoading(false);
+		}
+	};
+
 	return (
 		<Dialog open={isModalOpen} onOpenChange={onClose}>
 			<DialogContent className="bg-white text-black p-0 overflow-hidden">
@@ -52,8 +68,9 @@ const InviteModal = () => {
 						<Input
 							className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
 							value={inviteUrl}
+							disabled={isLoading}
 						/>
-						<Button size="icon" onClick={onCopy}>
+						<Button size="icon" onClick={onCopy} disabled={isLoading}>
 							{copied ? (
 								<Check className="w-4 h-4" />
 							) : (
@@ -62,6 +79,8 @@ const InviteModal = () => {
 						</Button>
 					</div>
 					<Button
+						onClick={onNew}
+						disabled={isLoading}
 						variant="link"
 						size="sm"
 						className="text-xs text-zinc-500 mt-4"
