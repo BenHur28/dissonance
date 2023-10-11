a77913e99e8beb36d22e0cebbafc0a89cda7066e
Ben Hur <bhur777@gmail.com>
2023-10-11 03:07:37 -0400

    Finish PATCH route

--- app/api/servers/[serverId]/route.ts
+++ app/api/servers/[serverId]/route.ts
@@ -1,3 +1,5 @@
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
 import { NextResponse } from "next/server";
 
 export async function PATCH(
@@ -5,6 +7,24 @@ export async function PATCH(
 	{ params }: { params: { serverId: string } }
 ) {
 	try {
+		const { name, imageUrl } = await req.json();
+		const profile = await currentProfile();
+		if (!profile) {
+			return new NextResponse("Unauthorized", { status: 401 });
+		}
+
+		const server = await db.server.update({
+			where: {
+				id: params.serverId,
+				profileId: profile.id,
+			},
+			data: {
+				name: name,
+				imageUrl: imageUrl,
+			},
+		});
+
+		return NextResponse.json(server);
 	} catch (error) {
 		console.log("SERVER_ID_PATCH", error);
 		return new NextResponse("Internal Error", { status: 500 });
