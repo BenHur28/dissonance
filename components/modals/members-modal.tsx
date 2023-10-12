154418f485f567fa6e4fbffc01ed2c2796f4f2b6
Ben Hur <bhur777@gmail.com>
2023-10-12 01:51:48 -0400

    Edit members modal

--- components/modals/members-modal.tsx
+++ components/modals/members-modal.tsx
@@ -12,44 +12,14 @@ import { Label } from "../ui/label";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 import { Check, Copy, RefreshCw } from "lucide-react";
-import { useOrigin } from "@/hooks/use-origin";
 import { useState } from "react";
 import axios from "axios";
 
 const MembersModal = () => {
 	const { onOpen, isOpen, onClose, type, data } = useModal();
-	const origin = useOrigin();
 
 	const isModalOpen = isOpen && type === "members";
 	const { server } = data;
-
-	const [copied, setCopied] = useState(false);
-	const [isLoading, setIsLoading] = useState(false);
-
-	const inviteUrl = `${origin}/invite/${server?.inviteCode}`;
-
-	const onCopy = () => {
-		navigator.clipboard.writeText(inviteUrl);
-		setCopied(true);
-
-		setTimeout(() => {
-			setCopied(false);
-		}, 2000);
-	};
-
-	const onNew = async () => {
-		try {
-			setIsLoading(true);
-			const response = await axios.patch(
-				`/api/servers/${server?.id}/invite-code`
-			);
-			onOpen("invite", { server: response.data });
-		} catch (error) {
-			console.log(error);
-		} finally {
-			setIsLoading(false);
-		}
-	};
 
 	return (
 		<Dialog open={isModalOpen} onOpenChange={onClose}>
@@ -59,35 +29,7 @@ const MembersModal = () => {
 						Invite Friends
 					</DialogTitle>
 				</DialogHeader>
-				<div className="p-6">
-					<Label className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70">
-						Server Invite link
-					</Label>
-					<div className="flex items-center mt-2 gap-x-2">
-						<Input
-							className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
-							value={inviteUrl}
-							disabled={isLoading}
-						/>
-						<Button size="icon" onClick={onCopy} disabled={isLoading}>
-							{copied ? (
-								<Check className="w-4 h-4" />
-							) : (
-								<Copy className="w-4 h-4" />
-							)}
-						</Button>
-					</div>
-					<Button
-						onClick={onNew}
-						disabled={isLoading}
-						variant="link"
-						size="sm"
-						className="text-xs text-zinc-500 mt-4"
-					>
-						Generate a new link!
-						<RefreshCw className="w-4 h-4 ml-2" />
-					</Button>
-				</div>
+				<div className="p-6">Hello Members List</div>
 			</DialogContent>
 		</Dialog>
 	);
