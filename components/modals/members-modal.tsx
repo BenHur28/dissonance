de93adc719673ac9fb18e0f3e233be5785f17645
Ben Hur <bhur777@gmail.com>
2023-10-12 02:19:45 -0400

    Show member name and role icon

--- components/modals/members-modal.tsx
+++ components/modals/members-modal.tsx
@@ -12,6 +12,13 @@ import { useModal } from "@/hooks/use-modal-store";
 import { ServerWithMembersWithProfiles } from "@/types";
 import { ScrollArea } from "../ui/scroll-area";
 import UserAvatar from "../user-avatar";
+import { ShieldAlert, ShieldCheck } from "lucide-react";
+
+const roleIconMap = {
+	GUEST: null,
+	MODERATOR: <ShieldCheck className="h-4 w-4 ml-2 text-indigo-500" />,
+	ADMIN: <ShieldAlert className="h-4 w-4 text-rose-500" />,
+};
 
 const MembersModal = () => {
 	const { onOpen, isOpen, onClose, type, data } = useModal();
@@ -21,7 +28,7 @@ const MembersModal = () => {
 
 	return (
 		<Dialog open={isModalOpen} onOpenChange={onClose}>
-			<DialogContent className="bg-white text-black p-0 overflow-hidden">
+			<DialogContent className="bg-white text-black overflow-hidden">
 				<DialogHeader className="pt-8 px-6">
 					<DialogTitle className="text-center text-2xl font-bold">
 						Manage Members
@@ -32,11 +39,14 @@ const MembersModal = () => {
 				</DialogHeader>
 				<ScrollArea className="mt-8 max-h-[420px] pr-6">
 					{server?.members?.map((member) => (
-						<div
-							key={member.id}
-							className="flex flex-items-center gap-x-2 mb-6"
-						>
+						<div key={member.id} className="flex items-center gap-x-2 mb-6">
 							<UserAvatar src={member.profile.imageUrl} />
+							<div className="flex flex-col gap-y-1">
+								<div className="text-sm font-semibold flex items-center gap-x-1">
+									{member.profile.name}
+									{roleIconMap[member.role]}
+								</div>
+							</div>
 						</div>
 					))}
 				</ScrollArea>
