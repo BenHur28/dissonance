a831c0a39918a6b38309758ce5044de149895e90
Ben Hur <bhur777@gmail.com>
2023-10-16 03:25:52 -0400

    Display video and audio channels

--- components/server/server-channel.tsx
+++ components/server/server-channel.tsx
@@ -2,8 +2,9 @@
 
 import { cn } from "@/lib/utils";
 import { Channel, ChannelType, MemberRole, Server } from "@prisma/client";
-import { Hash, Mic, Video } from "lucide-react";
+import { Edit, Hash, Mic, Trash, Video } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
+import { ActionTooltip } from "../action-tooltip";
 
 type ServerChannelProps = {
 	channel: Channel;
@@ -27,10 +28,30 @@ const ServerChannel = ({ channel, server, role }: ServerChannelProps) => {
 		<button
 			onClick={() => {}}
 			className={cn(
-				"group px-2 py-2 rounded-md flex items-center gap-x-2 mb-1 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition"
+				"group px-2 py-2 rounded-md flex items-center gap-x-2 mb-1 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition",
+				params?.channelId === channel.id && "bg-zinc-700/20 dark:bg-zinc-700"
 			)}
 		>
 			<Icon className="flex-shrink-0 w-5 h-5 text-zinc-500 dark:text-zinc-400" />
+			<p
+				className={cn(
+					"line-clamp-1 font-semibold text-sm text-zinc-500 group-hover:text-zinc-600 dark:text-zinc-600 dark:group-hover:text-zinc-300 transition",
+					params?.channelId === channel.id &&
+						"text-primary dark:text-zinc-200 dark:group-hover:text-white"
+				)}
+			>
+				{channel.name}
+			</p>
+			{channel.name !== "general" && role !== MemberRole.GUEST && (
+				<div className="ml-auto flex items-center gap-x-2">
+					<ActionTooltip label="Edit">
+						<Edit className="h-4 w-4 hidden group-hover:block text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition" />
+					</ActionTooltip>
+					<ActionTooltip label="Delete">
+						<Trash className="h-4 w-4 hidden group-hover:block text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition" />
+					</ActionTooltip>
+				</div>
+			)}
 		</button>
 	);
 };
