e4817a73f0d3fcf3368ae29cf9cfa28361dc3ea0
Ben Hur <bhur777@gmail.com>
2023-10-16 03:06:09 -0400

    display Icon for channel

--- components/server/server-channel.tsx
+++ components/server/server-channel.tsx
@@ -1,6 +1,9 @@
 "use client";
 
+import { cn } from "@/lib/utils";
 import { Channel, ChannelType, MemberRole, Server } from "@prisma/client";
+import { Hash, Mic, Video } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
 
 type ServerChannelProps = {
 	channel: Channel;
@@ -8,8 +11,28 @@ type ServerChannelProps = {
 	role?: MemberRole;
 };
 
+const iconMap = {
+	[ChannelType.TEXT]: Hash,
+	[ChannelType.AUDIO]: Mic,
+	[ChannelType.VIDEO]: Video,
+};
+
 const ServerChannel = ({ channel, server, role }: ServerChannelProps) => {
-	return <div>ServerChannel</div>;
+	const params = useParams();
+	const router = useRouter();
+
+	const Icon = iconMap[channel.type];
+
+	return (
+		<button
+			onClick={() => {}}
+			className={cn(
+				"group px-2 py-2 rounded-md flex items-center gap-x-2 mb-1 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition"
+			)}
+		>
+			<Icon className="flex-shrink-0 w-5 h-5 text-zinc-500 dark:text-zinc-400" />
+		</button>
+	);
 };
 
 export default ServerChannel;
