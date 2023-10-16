c67158c1d6f548cfdfc0c41b86aa309d2c44e49f
Ben Hur <bhur777@gmail.com>
2023-10-16 04:10:29 -0400

    add pre selection for channel creation

--- components/modals/create-channel-modal.tsx
+++ components/modals/create-channel-modal.tsx
@@ -36,6 +36,7 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { useModal } from "@/hooks/use-modal-store";
 import { ChannelType } from "@prisma/client";
+import { useEffect } from "react";
 
 const formSchema = z.object({
 	name: z
@@ -50,7 +51,8 @@ const formSchema = z.object({
 });
 
 const CreateChannelModal = () => {
-	const { isOpen, onClose, type } = useModal();
+	const { isOpen, onClose, type, data } = useModal();
+	const { channelType } = data;
 	const isModalOpen = isOpen && type === "createChannel";
 	const params = useParams();
 
@@ -59,9 +61,17 @@ const CreateChannelModal = () => {
 		resolver: zodResolver(formSchema),
 		defaultValues: {
 			name: "",
-			type: ChannelType.TEXT,
+			type: channelType || ChannelType.TEXT,
 		},
 	});
+
+	useEffect(() => {
+		if (channelType) {
+			form.setValue("type", channelType);
+		} else {
+			form.setValue("type", ChannelType.TEXT);
+		}
+	}, [channelType, form]);
 
 	const isLoading = form.formState.isSubmitting;
 
