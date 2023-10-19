f8866721c158103507c427fd9796e608ac9e075b
Ben Hur <bhur777@gmail.com>
2023-10-19 04:34:38 -0400

    Create mobile togger component

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -1,4 +1,5 @@
 import { Hash, Menu } from "lucide-react";
+import MobileToggle from "../mobile-toggle";
 
 type ChatHeaderProps = {
 	serverId: string;
@@ -13,7 +14,7 @@ const ChatHeader = ({ serverId, name, type, imageUrl }: ChatHeaderProps) => {
 			className="text-md font-semibold px-3 flex items-center h-12 
                         border-neutral-200 dark:border-neutral-800 border-b-2"
 		>
-			<Menu />
+			<MobileToggle />
 			{type === "channel" && (
 				<Hash className="w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2" />
 			)}
---
+++ components/mobile-toggle.tsx
@@ -0,0 +1,11 @@
+import { Menu } from "lucide-react";
+
+const MobileToggle = () => {
+	return (
+		<div>
+			<Menu />
+		</div>
+	);
+};
+
+export default MobileToggle;
