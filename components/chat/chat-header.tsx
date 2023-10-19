b4b3770348bc08e68a52b773a5415ffb0ef68ca0
Ben Hur <bhur777@gmail.com>
2023-10-19 04:38:19 -0400

    Finish mobile togger component

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -14,7 +14,7 @@ const ChatHeader = ({ serverId, name, type, imageUrl }: ChatHeaderProps) => {
 			className="text-md font-semibold px-3 flex items-center h-12 
                         border-neutral-200 dark:border-neutral-800 border-b-2"
 		>
-			<MobileToggle />
+			<MobileToggle serverId={serverId} />
 			{type === "channel" && (
 				<Hash className="w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2" />
 			)}
--- components/mobile-toggle.tsx
+++ components/mobile-toggle.tsx
@@ -1,9 +1,25 @@
 import { Menu } from "lucide-react";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Button } from "./ui/button";
+import NavSidebar from "./navigation/nav-sidebar";
+import ServerSideBar from "./server/server-sidebar";
 
-const MobileToggle = () => {
+const MobileToggle = ({ serverId }: { serverId: string }) => {
 	return (
 		<div>
-			<Menu />
+			<Sheet>
+				<SheetTrigger asChild>
+					<Button variant="ghost" size="icon" className="md:hidden">
+						<Menu />
+					</Button>
+				</SheetTrigger>
+				<SheetContent side="left" className="flex p-0 gap-0">
+					<div className="w-[72px]">
+						<NavSidebar />
+					</div>
+					<ServerSideBar serverId={serverId} />
+				</SheetContent>
+			</Sheet>
 		</div>
 	);
 };
