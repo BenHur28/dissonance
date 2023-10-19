a076652a653888c68c25925c9e67cc3d74efa6ed
Ben Hur <bhur777@gmail.com>
2023-10-19 04:31:54 -0400

    Style header

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -1,3 +1,5 @@
+import { Hash, Menu } from "lucide-react";
+
 type ChatHeaderProps = {
 	serverId: string;
 	name: string;
@@ -6,7 +8,17 @@ type ChatHeaderProps = {
 };
 
 const ChatHeader = ({ serverId, name, type, imageUrl }: ChatHeaderProps) => {
-	return <div></div>;
+	return (
+		<div
+			className="text-md font-semibold px-3 flex items-center h-12 
+                        border-neutral-200 dark:border-neutral-800 border-b-2"
+		>
+			<Menu />
+			{type === "channel" && (
+				<Hash className="w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2" />
+			)}
+		</div>
+	);
 };
 
 export default ChatHeader;
