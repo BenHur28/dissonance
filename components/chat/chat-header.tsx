71ae56ec6b0d9ce7337f9b10786a98596581b572
Ben Hur <bhur777@gmail.com>
2023-10-19 04:29:24 -0400

    type chat header component

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -1,5 +1,12 @@
-const ChatHeader = () => {
-	return <div>ChatHeader</div>;
+type ChatHeaderProps = {
+	serverId: string;
+	name: string;
+	type: "channel" | "conversation";
+	imageUrl?: string;
+};
+
+const ChatHeader = ({ serverId, name, type, imageUrl }: ChatHeaderProps) => {
+	return <div></div>;
 };
 
 export default ChatHeader;
