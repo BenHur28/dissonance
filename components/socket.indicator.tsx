51758cc9c6810200908f3352bec771a514f9bf34
Ben Hur <bhur777@gmail.com>
2023-10-22 01:09:49 -0400

    Create socket indictator component

---
+++ components/socket.indicator.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import { useSocket } from "./providers/socket.provider";
+import { Badge } from "./ui/badge";
+
+const SocketIndicator = () => {
+	return <div>socket.indicator</div>;
+};
+
+export default SocketIndicator;
