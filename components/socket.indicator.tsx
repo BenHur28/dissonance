b253fc52879ff5f429da3f60e1d460212b1c7208
Ben Hur <bhur777@gmail.com>
2023-10-22 01:11:22 -0400

    Finish socket indicator

--- components/socket.indicator.tsx
+++ components/socket.indicator.tsx
@@ -4,7 +4,21 @@ import { useSocket } from "./providers/socket.provider";
 import { Badge } from "./ui/badge";
 
 const SocketIndicator = () => {
-	return <div>socket.indicator</div>;
+	const { isConnected } = useSocket();
+
+	if (!isConnected) {
+		return (
+			<Badge variant="outline" className="bg-yellow-600 text-white border-none">
+				Fallback: Polling every 1s
+			</Badge>
+		);
+	}
+
+	return (
+		<Badge variant="outline" className="bg-emerald-600 text-white border-none">
+			Live: Real-time updates
+		</Badge>
+	);
 };
 
 export default SocketIndicator;
