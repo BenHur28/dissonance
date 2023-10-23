811b9614dece94ea38c8b7ffc5238b363ca39fe8
Ben Hur <bhur777@gmail.com>
2023-10-23 02:48:53 -0400

    Add placeholder text

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -54,8 +54,11 @@ const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
 									<Input
 										disabled={isLoading}
 										className="px-14 py-6 bg-zinc-200/90 dark:bg-zinc-700/75 
-                                    border-none border-0 text-zinc-600 dark:text-zinc-200
-                                    focus-visible:ring-0 focus-visible:ring-offset-0"
+                                                    border-none border-0 text-zinc-600 dark:text-zinc-200
+                                                    focus-visible:ring-0 focus-visible:ring-offset-0"
+										placeholder={`Message ${
+											type === "conversation" ? name : "#" + name
+										}`}
 									/>
 									<div className="absolute top-7 right-8">
 										<Smile />
