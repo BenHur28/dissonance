09b7cb0e2d982b2b596bd6c41b2c42b025826850
Ben Hur <bhur777@gmail.com>
2023-10-23 02:35:07 -0400

    add type for chatinput

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,4 +1,5 @@
 import ChatHeader from "@/components/chat/chat-header";
+import ChatInput from "@/components/chat/chat-input";
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { redirectToSignIn } from "@clerk/nextjs";
@@ -43,6 +44,7 @@ const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
 				type="channel"
 			/>
 			<div className="flex-1">Future Messages</div>
+			<ChatInput />
 		</div>
 	);
 };
--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -1,5 +1,12 @@
 "use client";
 
+type ChatInputProps = {
+	apiUrl: string;
+	query: Record<string, any>;
+	name: string;
+	type: "conversation" | "channel";
+};
+
 const ChatInput = () => {
 	return <div>ChatInput</div>;
 };
