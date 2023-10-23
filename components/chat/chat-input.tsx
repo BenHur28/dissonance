ff5a13c35d9858267913937e51cfeb06a09696c4
Ben Hur <bhur777@gmail.com>
2023-10-23 02:45:35 -0400

    Add FORM components and style

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -3,6 +3,9 @@
 import { useForm } from "react-hook-form";
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Plus } from "lucide-react";
 
 type ChatInputProps = {
 	apiUrl: string;
@@ -29,7 +32,33 @@ const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
 		console.log(value);
 	};
 
-	return <div>ChatInput</div>;
+	return (
+		<Form {...form}>
+			<form onSubmit={form.handleSubmit(onSubmit)}>
+				<FormField
+					control={form.control}
+					name="content"
+					render={({ field }) => (
+						<FormItem>
+							<FormControl>
+								<div className="relative p-4 pb-6">
+									<button
+										type="button"
+										onClick={() => {}}
+										className="absolute top-7 left-8 h-[24px] w-[24px] 
+                                        bg-zinc-500 dark:bg-zinc-400 hover:bg-zinc-600 dark:hover:bg-zinc-300 transition rounded-full p-1 
+                                        flex items-center justify-center"
+									>
+										<Plus className="text-white dark:text-[#313338]" />
+									</button>
+								</div>
+							</FormControl>
+						</FormItem>
+					)}
+				/>
+			</form>
+		</Form>
+	);
 };
 
 export default ChatInput;
