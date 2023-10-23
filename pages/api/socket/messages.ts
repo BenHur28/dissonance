98b4291eac3c1ef973e27e6f6751b92a7064c253
Ben Hur <bhur777@gmail.com>
2023-10-23 03:32:53 -0400

    get server

--- pages/api/socket/messages.ts
+++ pages/api/socket/messages.ts
@@ -1,3 +1,5 @@
+import { currentProfilePages } from "@/lib/current-profile-pages";
+import { db } from "@/lib/db";
 import { NextApiResponseServerIo } from "@/types";
 import { NextApiRequest } from "next";
 
@@ -10,6 +12,43 @@ export default async function handler(
 	}
 
 	try {
+		const profile = await currentProfilePages(req);
+		const { content, fileUrl } = req.body;
+		const { serverId, channelId } = req.query;
+
+		if (!profile) {
+			return res.status(401).json({ error: "Unauthorized" });
+		}
+
+		if (!serverId) {
+			return res.status(400).json({ error: "Server ID missing" });
+		}
+
+		if (!channelId) {
+			return res.status(400).json({ error: "Channel ID missing" });
+		}
+
+		if (!content) {
+			return res.status(400).json({ error: "Content missing" });
+		}
+
+		const server = await db.server.findFirst({
+			where: {
+				id: serverId as string,
+				members: {
+					some: {
+						profileId: profile.id,
+					},
+				},
+			},
+			include: {
+				members: true,
+			},
+		});
+
+		if (!server) {
+			res.status(404).json({ message: "Server not found" });
+		}
 	} catch (error) {
 		console.log("[MESSAGES_POST]", error);
 		return res.status(500).json({ message: "Internal Error" });
