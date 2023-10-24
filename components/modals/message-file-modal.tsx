4a2c7e3ca4cfd696231f036b7c01f1c12554ea97
Ben Hur <bhur777@gmail.com>
2023-10-24 02:35:51 -0400

    Change onSubmit and api route

--- components/modals/message-file-modal.tsx
+++ components/modals/message-file-modal.tsx
@@ -2,9 +2,9 @@
 
 import * as z from "zod";
 import axios from "axios";
+import qs from "query-string";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 
 import {
@@ -16,31 +16,21 @@ import {
 	DialogTitle,
 } from "@/components/ui/dialog";
 
-import {
-	Form,
-	FormControl,
-	FormField,
-	FormItem,
-	FormLabel,
-	FormMessage,
-} from "@/components/ui/form";
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
 
-import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { FileUpload } from "@/components/file-upload";
 import { useModal } from "@/hooks/use-modal-store";
 
 const formSchema = z.object({
-	name: z.string().min(1, {
-		message: "Server name is required.",
-	}),
-	imageUrl: z.string().min(1, {
-		message: "Server image is required.",
+	fileUrl: z.string().min(1, {
+		message: "Attachment is required.",
 	}),
 });
 
 const MessageFileModal = () => {
 	const { isOpen, onClose, type, data } = useModal();
+	const { apiUrl, query } = data;
 	const router = useRouter();
 
 	const isModalOpen = isOpen && type === "messageFile";
@@ -48,8 +38,7 @@ const MessageFileModal = () => {
 	const form = useForm({
 		resolver: zodResolver(formSchema),
 		defaultValues: {
-			name: "",
-			imageUrl: "",
+			fileUrl: "",
 		},
 	});
 
@@ -62,10 +51,15 @@ const MessageFileModal = () => {
 
 	const onSubmit = async (values: z.infer<typeof formSchema>) => {
 		try {
-			await axios.post("/api/servers", values);
+			const url = qs.stringifyUrl({
+				url: apiUrl || "",
+				query: query,
+			});
+
+			await axios.post(url, { ...values, content: values.fileUrl });
 			form.reset();
 			router.refresh();
-			window.location.reload();
+			onClose();
 		} catch (error) {
 			console.log(error);
 		}
@@ -76,11 +70,10 @@ const MessageFileModal = () => {
 			<DialogContent className="bg-white text-black p-0 overflow-hidden">
 				<DialogHeader className="pt-8 px-6">
 					<DialogTitle className="text-center text-2xl font-bold">
-						Customize your server
+						Add an attachment
 					</DialogTitle>
 					<DialogDescription className="text-center text-zinc-500">
-						Give your server a personality with a name and an image. You can
-						always change it later.
+						Send a file as a message
 					</DialogDescription>
 				</DialogHeader>
 				<Form {...form}>
@@ -89,12 +82,12 @@ const MessageFileModal = () => {
 							<div className="flex items-center justify-center text-center">
 								<FormField
 									control={form.control}
-									name="imageUrl"
+									name="fileUrl"
 									render={({ field }) => (
 										<FormItem>
 											<FormControl>
 												<FileUpload
-													endpoint="serverImage"
+													endpoint="messageFile"
 													value={field.value}
 													onChange={field.onChange}
 												/>
@@ -103,30 +96,10 @@ const MessageFileModal = () => {
 									)}
 								></FormField>
 							</div>
-							<FormField
-								control={form.control}
-								name="name"
-								render={({ field }) => (
-									<FormItem>
-										<FormLabel className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70">
-											Server name
-										</FormLabel>
-										<FormControl>
-											<Input
-												disabled={isLoading}
-												className="text-black bg-zinc-300/50 border-0 focus-visible:ring-0 focus-visible:ring-offset-0"
-												placeholder="Enter server name"
-												{...field}
-											/>
-										</FormControl>
-										<FormMessage />
-									</FormItem>
-								)}
-							/>
 						</div>
 						<DialogFooter className="bg-gray-100 px-6 py-4">
 							<Button disabled={isLoading} variant="primary">
-								Create
+								Send
 							</Button>
 						</DialogFooter>
 					</form>
