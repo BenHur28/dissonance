993210f741c0776bec22ef3d2254bf9f6651e650
Ben Hur <bhur777@gmail.com>
2023-10-25 05:44:46 -0400

    refresh page on submit

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -5,11 +5,12 @@ import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { Plus, Smile } from "lucide-react";
+import { Plus } from "lucide-react";
 import qs from "query-string";
 import axios from "axios";
 import { useModal } from "@/hooks/use-modal-store";
 import EmojiPicker from "../emoji-picker";
+import { useRouter } from "next/navigation";
 
 type ChatInputProps = {
 	apiUrl: string;
@@ -24,6 +25,7 @@ const formSchema = z.object({
 
 const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
 	const { onOpen } = useModal();
+	const router = useRouter();
 	const form = useForm<z.infer<typeof formSchema>>({
 		resolver: zodResolver(formSchema),
 		defaultValues: {
@@ -42,6 +44,7 @@ const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
 
 			await axios.post(url, values);
 			form.reset();
+			router.refresh();
 		} catch (error) {
 			console.log(error);
 		}
