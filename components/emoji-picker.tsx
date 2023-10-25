ac69656cb5cdbf29bef5bce199a5a47541effed6
Ben Hur <bhur777@gmail.com>
2023-10-25 05:40:20 -0400

    Add popver and style

--- components/emoji-picker.tsx
+++ components/emoji-picker.tsx
@@ -1,11 +1,29 @@
 "use client";
 
+import {
+	Popover,
+	PopoverContent,
+	PopoverTrigger,
+} from "@/components/ui/popover";
+import { Smile } from "lucide-react";
+
 type EmojiPickerProps = {
 	onChange: (value: string) => void;
 };
 
 const EmojiPicker = ({ onChange }: EmojiPickerProps) => {
-	return <div></div>;
+	return (
+		<Popover>
+			<PopoverTrigger>
+				<Smile className="text-zinc-500 dark:text-zinc-400 hover:text-zinc-600 dark:hover:text-zinc-300" />
+			</PopoverTrigger>
+			<PopoverContent
+				side="right"
+				sideOffset={40}
+				className="bg-transparent border-none shadow-none drop-shadow-none mb-16"
+			></PopoverContent>
+		</Popover>
+	);
 };
 
 export default EmojiPicker;
