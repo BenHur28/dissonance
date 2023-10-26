1b26653889d817a549c93a81c6f37d46a44f5d85
Ben Hur <bhur777@gmail.com>
2023-10-26 02:05:36 -0400

    type chat messages

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -1,5 +1,19 @@
 "use client";
 
+import { Member } from "@prisma/client";
+
+type ChatMessagesProps = {
+	name: string;
+	member: Member;
+	chatId: string;
+	apiUrl: string;
+	socketUrl: string;
+	socketQuery: Record<string, string>;
+	paramKey: "channelId" | "conversationId";
+	paramValue: string;
+	type: "channel" | "conversation";
+};
+
 const ChatMessages = () => {
 	return <div>ChatMessages</div>;
 };
