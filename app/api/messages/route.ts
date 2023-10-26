8bf6b385922f3d1b4ba1b359a4da46cfa8522903
Ben Hur <bhur777@gmail.com>
2023-10-26 05:42:02 -0400

    Create messages api route

---
+++ app/api/messages/route.ts
@@ -0,0 +1,7 @@
+import { NextResponse } from "next/server";
+import { Message } from "@prisma/client";
+
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+
+export async function GET(req: Request) {}
