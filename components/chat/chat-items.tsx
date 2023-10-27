98cd0e517b6f0ad09ed0471c5dc24434e30c0362
Ben Hur <bhur777@gmail.com>
2023-10-27 05:54:23 -0400

    Finish onsSubmit function for edit

--- components/chat/chat-items.tsx
+++ components/chat/chat-items.tsx
@@ -74,8 +74,21 @@ const ChatItem = ({
 		},
 	});
 
-	const onSubmit = (values: any) => {
-		console.log(values);
+	const isLoading = form.formState.isSubmitting;
+	const onSubmit = async (values: z.infer<typeof formSchema>) => {
+		try {
+			const url = qs.stringifyUrl({
+				url: `${socketUrl}/${id}`,
+				query: socketQuery,
+			});
+
+			await axios.patch(url, values);
+
+			form.reset();
+			setIsEditing(false);
+		} catch (error) {
+			console.log(error);
+		}
 	};
 
 	useEffect(() => {
@@ -171,6 +184,7 @@ const ChatItem = ({
 											<FormControl>
 												<div className="relative w-full">
 													<Input
+														disabled={isLoading}
 														className="p-2 bg-zinc-200/90 dark:bg-zinc-700/75 border-none border-0 
                                                     focus-visible:ring-0 focus-visible:ring-offset-0 text-zinc-600 dark:text-zinc-200"
 														placeholder="Edited message"
@@ -181,7 +195,7 @@ const ChatItem = ({
 										</FormItem>
 									)}
 								/>
-								<Button size="sm" variant="primary">
+								<Button disabled={isLoading} size="sm" variant="primary">
 									Save
 								</Button>
 							</form>
